aedc8aea0c70626ec9bebcf8cda6072171aa3d5e
ZeeksX <ikinwotezekiel@gmail.com>
2024-07-04 20:04:57 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="home">
-
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-}
-</script>
 
