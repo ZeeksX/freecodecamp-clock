4bae2b1beeffea943e897232664513bb6cb411ee
ZeeksX <ikinwotezekiel@gmail.com>
2024-07-05 01:19:49 +0100

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,29 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', {
-  state: () => ({ 
+export const useCounterStore = defineStore("counter", {
+  state: () => ({
     break: 5,
     session: 25,
     minute: 0,
-    state: "Session"
+    state: "Session",
   }),
-  getters: {
-    
-  },
+  getters: {},
   actions: {
-    
+    increaseBreakTime() {
+      if(this.break<=59) this.break += 1;
+    },
+    decreaseBreakTime() {
+      if(this.break>1) this.break -= 1;
+    },
+    increaseSessionTime() {
+      if(this.session<=59) this.session += 1;
+    },
+    decreaseSessionTime() {
+      if(this.session>1) this.session -= 1;
+    },
+    reset(){
+      this.break = 5,
+      this.session = 25
+    }
   },
-})
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,15 @@
         <h3 id="break-label">Break Length</h3>
         <div class="break-controls">
           <div>
-            <h3 id="break-decrement"><i class="fa-solid fa-arrow-down fa-2x"></i></h3>
+            <h3 id="break-decrement"><i class="fa-solid fa-arrow-down fa-2x"
+                @click="counterStore.decreaseBreakTime"></i></h3>
           </div>
           <div>
             <h3 id="break-length">{{ counterStore.break }}</h3>
           </div>
           <div>
-            <h3 id="break-increment"><i class="fa-solid fa-arrow-up fa-2x"></i></h3>
+            <h3 id="break-increment" @click="counterStore.increaseBreakTime"><i class="fa-solid fa-arrow-up fa-2x"></i>
+            </h3>
           </div>
         </div>
       </div>
@@ -20,27 +22,29 @@
         <h3 id="session-label">Session Length</h3>
         <div class="session-controls">
           <div>
-            <h3 id="session-decrement"><i class="fa-solid fa-arrow-down fa-2x"></i></h3>
+            <h3 id="session-decrement"><i class="fa-solid fa-arrow-down fa-2x"
+                @click="counterStore.decreaseSessionTime"></i></h3>
           </div>
           <div>
             <h3 id="session-length">{{ counterStore.session }}</h3>
           </div>
           <div>
-            <h3 id="session-increment"><i class="fa-solid fa-arrow-up fa-2x"></i></h3>
+            <h3 id="session-increment"><i class="fa-solid fa-arrow-up fa-2x"
+                @click="counterStore.increaseSessionTime"></i></h3>
           </div>
         </div>
       </div>
     </div>
     <div class="timer">
       <h3 id="timer-label">{{ counterStore.state }}</h3>
-      <h1 id="time-left"> {{ counterStore.session }}:{{ addZero(counterStore.minute) }} </h1>
+      <h1 id="time-left"> {{ addZero(counterStore.session) }}:{{ addZero(counterStore.minute) }} </h1>
     </div>
     <div class="timer-controls">
       <div class="play-stop">
         <i class="fa-solid fa-play fa-2x"></i>
         <i class="fa-solid fa-pause fa-2x"></i>
       </div>
-      <i class="fa-solid fa-rotate fa-2x"></i>
+      <i class="fa-solid fa-rotate fa-2x" @click="counterStore.reset"></i>
     </div>
   </div>
   <footer>
